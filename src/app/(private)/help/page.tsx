e43fb245ce4ca99bedc2571278de1117bc8edca2
camilosanchezdev <camilosanchezdev@gmail.com>
2024-08-13 16:17:30 -0300

    feat: add pages

---
+++ src/app/(private)/help/page.tsx
@@ -0,0 +1,17 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function HelpPage() {
+  return (
+    <Box sx={{ margin: 4 }}>
+      <Typography
+        variant="h4"
+        component="h1"
+        noWrap
+        sx={{ margin: '16px 0', fontWeight: 'bold', color: '#444' }}
+      >
+        Help
+      </Typography>
+    </Box>
+  );
+}
